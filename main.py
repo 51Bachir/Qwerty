2d51bf1964eb89fce21dcd4c068d1f69683a7dfb
Mohamed Sibachir <75868538+51Bachir@users.noreply.github.com>
2023-07-17 03:35:54 -0400

    Update main.py
    In this enhanced version:
    
    The port_scan() function uses the nmap library to perform a comprehensive port scan on the target IP address. It uses more advanced scanning options (-A -v) to gather additional information about open ports.
    
    The vulnerability_scan() function retrieves vulnerability information from an online vulnerability database based on the target IP address. It uses the requests library to fetch the HTML page and BeautifulSoup to parse the page and extract relevant vulnerability data.
    
    The generate_report() function generates a detailed report based on the scan results. It presents open ports and detected vulnerabilities in a structured manner.

--- main.py
+++ main.py
@@ -1,35 +1,73 @@
-import socket
-from scapy.all import *
+import nmap
+import requests
+from bs4 import BeautifulSoup
+from scapy.all import sr1, IP, TCP
 
-# Scan common ports on a target IP address
+# Scan target IP address using Nmap
 def port_scan(target_ip):
-    common_ports = [21, 22, 23, 25, 53, 80, 110, 139, 443, 445]
+    scanner = nmap.PortScanner()
+    result = scanner.scan(target_ip, arguments='-p1-65535 -T4 -A -v')
 
     open_ports = []
 
-    for port in common_ports:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
-
-        result = sock.connect_ex((target_ip, port))
-
-        if result == 0:
-            open_ports.append(port)
-
-        sock.close()
+    for protocol in result['scan'][target_ip]:
+        for port in result['scan'][target_ip][protocol]:
+            state = result['scan'][target_ip][protocol][port]['state']
+            if state == 'open':
+                open_ports.append(port)
 
     return open_ports
 
-# Perform a vulnerability scan on a target IP address
+# Perform a vulnerability scan on target IP using online vulnerability databases
 def vulnerability_scan(target_ip):
-    # Implement your vulnerability scanning logic here
-    # This can include checking for known vulnerabilities in services running on the target IP address
+    url = f'https://www.cvedetails.com/google-search-results.php?q={target_ip}&sa=Submit'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Example: Checking if the target IP address is vulnerable to Heartbleed (CVE-2014-0160)
-    result = sr1(IP(dst=target_ip)/TCP(dport=443)/TLS(), verbose=0)
+    vulnerabilities = []
+    vuln_table = soup.find('table', {'id': 'vulnslisttable'})
 
-    if result and result[TLS].version == 'TLS 1.0':
-        print(f"Target IP: {target_ip} is vulnerable to Heartbleed (CVE-2014-0160)")
+    if vuln_table:
+        vuln_rows = vuln_table.find_all('tr')
+        for row in vuln_rows[1:]:
+            cols = row.find_all('td')
+            if len(cols) >= 4:
+                cve_id = cols[1].text.strip()
+                severity = cols[3].text.strip()
+                vulnerabilities.append((cve_id, severity))
+
+    return vulnerabilities
+
+# Send a TCP SYN packet to check if a specific port is open
+def check_port(target_ip, port):
+    packet = IP(dst=target_ip) / TCP(dport=port, flags="S")
+
+    response = sr1(packet, timeout=1, verbose=0)
+    if response and response.haslayer(TCP) and response[TCP].flags == "SA":
+        return True
+
+    return False
+
+# Generate a report based on the scan results
+def generate_report(target_ip, open_ports, vulnerabilities):
+    print("Network Penetration Test Report")
+    print("===============================")
+    print(f"Target IP: {target_ip}\n")
+
+    if open_ports:
+        print("Open Ports:")
+        for port in open_ports:
+            print(f" - Port {port} is open")
+
+    print()
+
+    if vulnerabilities:
+        print("Vulnerabilities:")
+        for cve_id, severity in vulnerabilities:
+            print(f" - CVE ID: {cve_id}, Severity: {severity}")
+
+    if not open_ports and not vulnerabilities:
+        print("No significant findings.")
 
 # Main function
 def main():
@@ -38,14 +76,14 @@ def main():
     print("\nPerforming port scan...")
     open_ports = port_scan(target_ip)
     if open_ports:
-        print("Open ports found on the target IP address:")
-        for port in open_ports:
-            print(f"Port {port} is open")
+        print("Open ports found on the target IP address.")
     else:
-        print("No open ports found on the target IP address")
+        print("No open ports found on the target IP address.")
 
     print("\nPerforming vulnerability scan...")
-    vulnerability_scan(target_ip)
+    vulnerabilities = vulnerability_scan(target_ip)
+
+    generate_report(target_ip, open_ports, vulnerabilities)
 
 if __name__ == '__main__':
     main()
